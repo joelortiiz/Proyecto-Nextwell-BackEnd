a9806eb12c8efe2652bebe75b40f94fc7575f6e1
joelortiiz <daw_joelortiz@riberadeltajo.es>
2024-05-31 21:22:20 +0200

    deleted unnecesary files

--- src/server.js
+++ src/server.js
@@ -1,11 +1,3 @@
-// // src/server.js
-// const app = require('./app');
-// const PORT = process.env.PORT || 3001;
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
 
 const express = require('express');
 const dataRoutes = require('./routes/dataRoutes');
