940b45ec56e1fae21800c32da4070b5f4433e5bc
joelortiiz <daw_joelortiz@riberadeltajo.es>
2024-06-01 17:28:23 +0200

    .

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -8,6 +8,16 @@ router.get('/cpus', fetchCpuData);
 router.get('/ssds', fetchSsdData);
 router.get('/os', getOSEditions);
 
+router.delete('/ordenadores/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+      await Ordenador.findByIdAndDelete(id);
+      res.status(200).json({ message: 'Ordenador eliminado correctamente' });
+    } catch (error) {
+      res.status(500).json({ message: 'Error eliminando el ordenador' });
+    }
+  });
+
 module.exports = router;
 
 
